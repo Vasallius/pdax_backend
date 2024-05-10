387fdcd43166baa205ccdb2a58239d261f6370d8
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2024-05-11 00:40:42 +0800

    feat: add create_account

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import uuid
+
+
 class Account:
-    def __init__(self, account_id, customer_id, account_number, balance):
+    def __init__(self, account_id, customer_id, balance):
         self.account_id = account_id
         self.customer_id = customer_id
-        self.account_number = account_number
+        # self.account_number = account_number
         self.balance = balance
 
 
@@ -12,3 +15,10 @@ class Customer:
         self.name = name
         self.email = email
         self.phone_number = phone_number
+
+
+class UseCase:
+    @staticmethod
+    def create_account(customer_id, name, email, phone_number):
+        account_id = str(uuid.uuid4())
+        return Account(account_id, customer_id, 0)
