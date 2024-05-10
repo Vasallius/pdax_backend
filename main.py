b6cd45f4202a7d2c0eeb51ec38b4de69dfe748a3
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2024-05-11 00:22:47 +0800

    feat: add customer class

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ class Account:
         self.customer_id = customer_id
         self.account_number = account_number
         self.balance = balance
+
+
+class Customer:
+    def __init__(self, customer_id, name, email, phone_number):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
