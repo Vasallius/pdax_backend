e8911f958b4b9db537c6f410a401eefa2491aa94
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2024-05-11 00:20:06 +0800

    init: initial Account class

---
+++ main.py
@@ -0,0 +1,6 @@
+class Account:
+    def __init__(self, account_id, customer_id, account_number, balance):
+        self.account_id = account_id
+        self.customer_id = customer_id
+        self.account_number = account_number
+        self.balance = balance
